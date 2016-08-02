290043bd21475babb571111397785f021d0dfbac
tuananh <tuananh05031997@gmail.com>
2016-08-02 15:29:47 +0700

    Init project

--- src/com/uet/anh/tank/object/ShotHit.java
+++ src/com/uet/anh/tank/object/ShotHit.java
@@ -6,15 +6,5 @@ import java.awt.*;
  * Created by tuana on 02/08/2016.
  */
 public class ShotHit {
-  public void shotBullet(EnemyTank enemy , PlayerTank player){
-      Rectangle rect1 = new Rectangle(player.getCoordinatesX(), player.getCoordinatesY(), player.SIZE_TANK, player.SIZE_TANK);
 
-      Rectangle rect2 = new Rectangle(enemy.getCoordinatesX(), enemy.getCoordinatesY(), enemy.SIZE_TANK, enemy.SIZE_TANK);
-
-//detects when the two rectangles hit
-      if(rect1.intersects(rect2))
-      {
-          System.out.println("game over, g");
-      }
-  }
 }
